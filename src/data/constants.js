d22204500b92abcd4b4080f0a4caec5dde452cf5
Denison Kolling <denisonkolling@gmail.com>
2024-01-30 18:18:02 -0300

    primeiro commit

---
+++ src/data/constants.js
@@ -0,0 +1,57 @@
+export const Cardapio = [
+	{
+		nome: 'Big Mac',
+		descricao: 'Dois hambúrgueres, alface, queijo, molho especial, cebola e picles em um pão com gergelim.',
+		valor: 15.9,
+    imagem: "link_da_imagem.jpg"
+	},
+	{
+		nome: 'McChicken',
+		descricao: 'Filé de frango empanado, alface, maionese especial em um pão com gergelim.',
+		valor: 12.5,
+    imagem: "link_da_imagem.jpg"
+	},
+	{
+		nome: 'McNuggets (6 peças)',
+		descricao: 'Pedacinhos de frango empanado acompanhados de molho barbecue.',
+		valor: 9.99,
+    imagem: "link_da_imagem.jpg"
+	},
+	{
+		nome: 'Batata Frita (média)',
+		descricao: 'Batatas fritas crocantes e douradas.',
+		valor: 7.0,
+    imagem: "link_da_imagem.jpg"
+	},
+	{
+		nome: 'Coca-Cola (500ml)',
+		descricao: 'Refrigerante Coca-Cola gelado.',
+		valor: 5.0,
+    imagem: "link_da_imagem.jpg"
+	},
+	{
+		nome: 'McFlurry Oreo',
+		descricao:
+			'Sorvete de baunilha com pedaços de biscoito Oreo e calda de chocolate.',
+		valor: 8.5,
+    imagem: "link_da_imagem.jpg"
+	},
+	{
+		nome: 'Apple Pie',
+		descricao: 'Torta de maçã quente e deliciosa.',
+		valor: 4.5,
+    imagem: "link_da_imagem.jpg"
+	},
+	{
+		nome: 'McShake de Morango',
+		descricao: 'Shake cremoso de morango.',
+		valor: 6.0,
+    imagem: "link_da_imagem.jpg"
+	},
+	{
+		nome: 'Água Mineral (500ml)',
+		descricao: 'Água mineral natural.',
+		valor: 3.0,
+    imagem: "link_da_imagem.jpg"
+	},
+];
